49caa0639b3436ace08ae3bc1a60553815513c12
mingyang.tan <mingyang.tan@mobvista.com>
2019-10-16 10:12:12 +0800

    update .md

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-import 'font-awesome/css/font-awesome.min.css'
-import './index.css'
 import './index.scss'
 
-ReactDOM.render(
-  <div>
-    <i className="fa fa-address-book"></i>
-    <h1>Hello, world!</h1>
-  </div>,
-  document.getElementById('app')
-)
+let jsx = <div>jsx..</div>
+
+ReactDOM.render(jsx, document.getElementById('app'));
